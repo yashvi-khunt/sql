d822ac53652557a2acc2cb3a02a1e507a96cb370
yashvi-khunt <yashvi@weybee.in>
2023-09-28 12:10:23 +0530

    completed user defined types

---
+++ Advance SQL/userDefinedTypes.sql
@@ -0,0 +1,20 @@
+---user defined types
+
+use MOVIES_W3
+
+create type actorType as table(firstName varchar(50),lastName varchar(50),age int)
+
+create procedure readActors
+@a actorType readonly
+as
+select * from @a
+return 0;
+
+declare @act as actorType
+insert into @act
+select act_fname,act_lname,10 from actor
+EXEC readActors @ACT	
+
+
+
+
